594b421d45003a7ed6d5cb5d3514e6ee55df9bb2
Swiftful Thinking <ndsarno16@gmail.com>
2023-01-25 00:46:40 -0500

    [Bug] Fix NavigationView on AnyDestination

--- Sources/SwiftfulRouting/Core/Router.swift
+++ Sources/SwiftfulRouting/Core/Router.swift
@@ -56,11 +56,11 @@ public class Router: ObservableObject {
             await MainActor.run(body: {
                 switch option {
                 case .push, .fullScreenCover:
-                    self.screen = AnyDestination(RouterView { router in
+                    self.screen = AnyDestination(SubRouterView { router in
                         destination(router)
                     })
                 case .sheet:
-                    self.screen = AnyDestination(SubRouterView { router in
+                    self.screen = AnyDestination(RouterView { router in
                         destination(router)
                     })
                 }
