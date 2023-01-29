96a7d74f04adfcf65871b4ac209dc5c6f2071085
Swiftful Thinking <ndsarno16@gmail.com>
2023-01-29 12:59:44 -0500

    [Feature] iOS 13 support

--- Sources/SwiftfulRouting/ViewModifiers/FullScreenCoverViewModifier.swift
+++ Sources/SwiftfulRouting/ViewModifiers/FullScreenCoverViewModifier.swift
@@ -8,18 +8,26 @@
 import Foundation
 import SwiftUI
 
-@available(iOS 14.0, *)
 struct FullScreenCoverViewModifier: ViewModifier {
     
     let option: SegueOption
     let screens: Binding<[AnyDestination]>
 
     func body(content: Content) -> some View {
-        content
-            .fullScreenCover(item: Binding(if: option, is: .fullScreenCover, value: Binding(toLastElementIn: screens)), onDismiss: nil) { _ in
-                if let view = screens.wrappedValue.last?.destination {
-                    view
+        if #available(iOS 14, *) {
+            content
+                .fullScreenCover(item: Binding(if: option, is: .fullScreenCover, value: Binding(toLastElementIn: screens)), onDismiss: nil) { _ in
+                    if let view = screens.wrappedValue.last?.destination {
+                        view
+                    }
                 }
-            }
+        } else {
+            content
+                .sheet(item: Binding(if: option, is: .fullScreenCover, value: Binding(toLastElementIn: screens)), onDismiss: nil) { destination in
+                    if let view = screens.wrappedValue.last?.destination {
+                        view
+                    }
+                }
+        }
     }
 }
