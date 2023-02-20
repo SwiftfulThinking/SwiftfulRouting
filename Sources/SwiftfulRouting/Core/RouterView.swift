8b8117e865ad4e329d6271c849f6934f00475248
Nick <ndsarno16@gmail.com>
2023-02-20 01:20:07 -0500

    i

--- Sources/SwiftfulRouting/Core/RouterView.swift
+++ Sources/SwiftfulRouting/Core/RouterView.swift
@@ -82,11 +82,11 @@ public struct RouterView<T:View>: View, Router {
         .showingAlert(option: alertOption, item: $alert)
         .showingModal(configuration: modalConfiguration, item: $modal)
         .onFirstAppear {
-            print("set stack count: \(screens.count)")
-            Task {
-                try? await Task.sleep(nanoseconds: 3_000_000_000)
-                screenStackCount = screens.count
-            }
+//            Task {
+//                try? await Task.sleep(nanoseconds: 3_000_000_000)
+                screenStackCount = screenStack.count
+                print("set stack count: \(screens.count)")
+//            }
         }
     }
     
