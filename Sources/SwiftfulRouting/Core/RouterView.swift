a53c45706f0c9b150ef2758d3d2e33a8e71f36cf
Nick <hello@swiftful-thinking.com>
2023-09-03 15:47:56 -0400

    2 step

--- Sources/SwiftfulRouting/Core/RouterView.swift
+++ Sources/SwiftfulRouting/Core/RouterView.swift
@@ -240,6 +240,7 @@ public struct RouterView<T:View>: View, Router {
             route.setDidSegueToTrue()
             localRoutes[index] = route
             print("SET: \(route.id) to TRUEEEEE")
+            routes = []
             routes = localRoutes
             print(localRoutes[index])
             print(routes[index])
