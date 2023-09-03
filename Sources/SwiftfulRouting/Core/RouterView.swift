1a92de796011613eba441246908cfb5a2de32c78
Nick <hello@swiftful-thinking.com>
2023-09-03 15:57:39 -0400

    routes

--- Sources/SwiftfulRouting/Core/RouterView.swift
+++ Sources/SwiftfulRouting/Core/RouterView.swift
@@ -237,17 +237,17 @@ public struct RouterView<T:View>: View, Router {
         Task {
             // Remove route
             // the problem is I am updates routes data model and it's not populating
-            var localRoutes: [AnyRoute] = routes
-            if let index = localRoutes.firstIndex(of: route) {
-                var route = localRoutes[index]
+//            var localRoutes: [AnyRoute] = routes
+            if let index = routes.firstIndex(of: route) {
+                var route = routes[index]
                 route.setDidSegueToTrue()
-                localRoutes[index] = route
+                routes[index] = route
                 print("SET: \(route.id) to TRUEEEEE")
-                routes = []
+//                routes = []
 //                try? await Task.sleep(nanoseconds: 1_000_000_000)
-                routes = localRoutes
+//                routes = localRoutes
 //                try? await Task.sleep(nanoseconds: 1_000_000_000)
-                print(localRoutes[index])
+//                print(localRoutes[index])
                 print(routes[index])
                 print("HERE")
             }
@@ -257,7 +257,7 @@ public struct RouterView<T:View>: View, Router {
                 // Sheet and FullScreenCover enter new Environments and require a new Navigation to be added, and don't need an environmentRouter because they will host the environment.
                 self.sheetDetents = [.large]
                 self.sheetSelectionEnabled = false
-                self.screens.append(AnyDestination(RouterView<V>(addNavigationView: true, screens: nil, route: route, routes: localRoutes, environmentRouter: nil, content: destination)))
+                self.screens.append(AnyDestination(RouterView<V>(addNavigationView: true, screens: nil, route: route, routes: routes, environmentRouter: nil, content: destination)))
             } else {
                 // Using existing Navigation
                 // Push continues in the existing Environment and uses the existing Navigation
@@ -267,16 +267,16 @@ public struct RouterView<T:View>: View, Router {
                 if #available(iOS 16, *) {
                     if screenStack.isEmpty {
                         // We are in the root Router and should start building on $screens
-                        self.screens.append(AnyDestination(RouterView<V>(addNavigationView: false, screens: $screens, route: route, routes: localRoutes, environmentRouter: environmentRouter, content: destination)))
+                        self.screens.append(AnyDestination(RouterView<V>(addNavigationView: false, screens: $screens, route: route, routes: routes, environmentRouter: environmentRouter, content: destination)))
                     } else {
                         // We are not in the root Router and should continue off of $screenStack
-                        self.screenStack.append(AnyDestination(RouterView<V>(addNavigationView: false, screens: $screenStack, route: route, routes: localRoutes, environmentRouter: environmentRouter, content: destination)))
+                        self.screenStack.append(AnyDestination(RouterView<V>(addNavigationView: false, screens: $screenStack, route: route, routes: routes, environmentRouter: environmentRouter, content: destination)))
                     }
                     
                 // iOS 14/15 uses NavigationView and can only push 1 view at a time
                 } else {
                     // Push a new screen and don't pass view stack to child view (screens == nil)
-                    self.screens.append(AnyDestination(RouterView<V>(addNavigationView: false, screens: nil, route: route, routes: localRoutes, environmentRouter: environmentRouter, content: destination)))
+                    self.screens.append(AnyDestination(RouterView<V>(addNavigationView: false, screens: nil, route: route, routes: routes, environmentRouter: environmentRouter, content: destination)))
                 }
             }
         }
