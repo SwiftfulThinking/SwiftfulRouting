1aa068602f106680c51237b1713c273dece541a9
Nick <hello@swiftful-thinking.com>
2024-01-14 19:02:43 -0500

    more

--- Sources/SwiftfulRouting/Core/RouterView.swift
+++ Sources/SwiftfulRouting/Core/RouterView.swift
@@ -396,6 +396,7 @@ public struct RouterView<T:View>: View, Router {
         }
         
         if let newRootScreen {
+            print("NEW ROOT: \(newRootScreen.id)")
             removeRoutes(route: newRootScreen)
         }
         
@@ -413,23 +414,23 @@ public struct RouterView<T:View>: View, Router {
         self.screens = []
         self.screenStack = []
 
-//        if !routes.isEmpty {
-//            print("ROUTES CONTAINS: \(routes.count)")
-//            for (index, route) in routes.enumerated() {
-//                for route2 in route {
-//                    print("\(index) :: \(route2.id) :: \(route2.segue)")
-//                }
-//            }
-//        } else {
-//            let allRoutesInFrontOfCurrent = routes.flatMap({ $0 }).allAfter(route)
-//
-//            print("ROOT ROUTES CONTAINS")
-//            for (index, route) in rootRoutes.enumerated() {
-//                for route2 in route {
-//                    print("\(index) :: \(route2.segue)")
-//                }
-//            }
-//        }
+        if !routes.isEmpty {
+            print("ROUTES CONTAINS: \(routes.count)")
+            for (index, route) in routes.enumerated() {
+                for route2 in route {
+                    print("\(index) :: \(route2.id) :: \(route2.segue)")
+                }
+            }
+        } else {
+            let allRoutesInFrontOfCurrent = routes.flatMap({ $0 }).allAfter(route)
+
+            print("ROOT ROUTES CONTAINS")
+            for (index, route) in rootRoutes.enumerated() {
+                for route2 in route {
+                    print("\(index) :: \(route2.segue)")
+                }
+            }
+        }
 //        print("currently in: \(route.id)")
 //        removeRoutes(route: self.route)
 
