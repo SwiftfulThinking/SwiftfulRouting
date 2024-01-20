514511c385d92047b881b1a6a0d5fd8bdc34833f
Nick <hello@swiftful-thinking.com>
2024-01-20 13:06:06 -0500

    attempt empty view

--- Sources/SwiftfulRouting/Core/RouterView.swift
+++ Sources/SwiftfulRouting/Core/RouterView.swift
@@ -74,7 +74,7 @@ struct RouterViewInternal<Content:View>: View, Router {
 //    @State private var modalConfiguration: ModalConfiguration = .default
 //    @State private var modal: AnyDestination? = nil
     
-    @State private var modals: [AnyDestination] = [] //AnyDestination(Text("HI BABE"))
+    @State private var modals: [AnyDestination] = [AnyDestination(EmptyView())]
         
     public init(addNavigationView: Bool = true, screens: (Binding<[AnyDestination]>)? = nil, route: AnyRoute? = nil, routes: Binding<[[AnyRoute]]>? = nil, environmentRouter: Router? = nil, @ViewBuilder content: @escaping (AnyRouter) -> Content) {
         self.addNavigationView = addNavigationView
