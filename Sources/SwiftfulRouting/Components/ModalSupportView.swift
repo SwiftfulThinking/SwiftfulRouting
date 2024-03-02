8dee615038f7cac4371a011aea3e4c4497b64b4c
Nick <hello@swiftful-thinking.com>
2024-03-01 20:01:22 -0500

    Access LazyZStack via SwiftfulRecursiveUI

--- Sources/SwiftfulRouting/Components/ModalSupportView.swift
+++ Sources/SwiftfulRouting/Components/ModalSupportView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import SwiftfulRecursiveUI
 
 struct AnyModalWithDestination: Identifiable, Equatable {
     let id: String
--- Sources/SwiftfulRouting/Components/TransitionSupportView.swift
+++ Sources/SwiftfulRouting/Components/TransitionSupportView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import SwiftfulRecursiveUI
 
 struct AnyTransitionWithDestination: Identifiable, Equatable {
     let id: String
