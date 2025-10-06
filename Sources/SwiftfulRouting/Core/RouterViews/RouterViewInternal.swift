29d293ba77290e4ce60dc2ae458b983478f58ecc
Nick <hello@swiftful-thinking.com>
2025-10-05 20:09:55 -0400

    First attempt

--- Sources/SwiftfulRouting/Core/RouterViews/RouterViewInternal.swift
+++ Sources/SwiftfulRouting/Core/RouterViews/RouterViewInternal.swift
@@ -56,17 +56,17 @@ struct RouterViewInternal<Content: View>: View, Router {
                     // There's a weird behavior (bug?) where the presentationDetent is not calculated
                     // If the .sheet modifier is outside of the NavigationStack
                     // Therefore, if we add NavigationStack, we add these as children of it
-                    .sheetBackgroundModifier(viewModel: viewModel, routerId: routerId)
-                    .fullScreenCoverBackgroundModifer(viewModel: viewModel, routerId: routerId)
+//                    .sheetBackgroundModifier(viewModel: viewModel, routerId: routerId)
+//                    .fullScreenCoverBackgroundModifer(viewModel: viewModel, routerId: routerId)
             }
         })
         
         // If we don't add NavigationStack, add .sheet modifiers here instead
-        .ifSatisfiesCondition(!addNavigationStack, transform: { content in
-            content
+//        .ifSatisfiesCondition(!addNavigationStack, transform: { content in
+//            content
                 .sheetBackgroundModifier(viewModel: viewModel, routerId: routerId)
                 .fullScreenCoverBackgroundModifer(viewModel: viewModel, routerId: routerId)
-        })
+//        })
         
         // If this is the root router, add "root" stack to the array
         .ifSatisfiesCondition(routerId == RouterViewModel.rootId, transform: { content in
